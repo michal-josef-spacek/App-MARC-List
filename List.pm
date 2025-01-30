059c117105d6123f0e6bda2dadd90f02b86a239c
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-01-30 14:06:19 +0100

    Prepare for 0.06 version

--- List.pm
+++ List.pm
@@ -11,7 +11,7 @@ use List::MoreUtils qw(uniq);
 use MARC::File::XML (BinaryEncoding => 'utf8', RecordFormat => 'MARC21');
 use Unicode::UTF8 qw(decode_utf8 encode_utf8);
 
-our $VERSION = 0.05;
+our $VERSION = 0.06;
 
 # Constructor.
 sub new {
@@ -358,6 +358,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.05
+0.06
 
 =cut
